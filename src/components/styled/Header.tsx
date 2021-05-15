49f8e8dfd1bcf8ddc970af3cce6726d9c50d9762
aidys23 <aidongak23@gmail.com>
2021-05-15 11:31:30 +0700

    add styled header

---
+++ src/components/styled/Header.tsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const Header = styled.h1`
+  color: #4ea5d9;
+  font-style: italic;
+`;
---
+++ src/components/styled/styled.stories.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { Header } from "./index";
+
+export default {
+  title: "Styled Components",
+};
+
+export const HeaderStory: FC = () => <Header>Header</Header>;
