7241d47947aadf9e45a58c472238a165531a4761
aidys23 <aidongak23@gmail.com>
2021-05-16 15:51:39 +0700

    test render ControlForm

--- src/components/ControlForm/ControlForm.test.tsx
+++ src/components/ControlForm/ControlForm.test.tsx
@@ -1,13 +1,26 @@
-describe('Control form', () => {
-  test('render test');
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import { ControlForm } from "./ControlForm";
+import "@testing-library/jest-dom";
 
-  test('should call running/stop handler');
+describe("Control form", () => {
+  test("render test", () => {
+    render(<ControlForm />);
+    const sizeLabel = screen.queryByText(/size/i);
+    const speedLabel = screen.queryByText(/speed/i);
+    const filledLabel = screen.queryByText(/filled/i);
+    expect(sizeLabel).toBeInTheDocument();
+    expect(speedLabel).toBeInTheDocument();
+    expect(filledLabel).toBeInTheDocument();
+  });
 
-  test('should call clear handler');
+  test("should call running/stop handler");
 
-  test('should call size handler');
+  test("should call clear handler");
 
-  test('should call speed handler');
+  test("should call size handler");
 
-  test('should call filled handler');
+  test("should call speed handler");
+
+  test("should call filled handler");
 });
