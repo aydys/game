7c2446ce2e92faa8c2891ab9a6950edc5732cc58
aidys23 <aidongak23@gmail.com>
2021-05-16 22:50:34 +0700

    add handlers to ControlForm

--- src/components/ControlForm/ControlForm.test.tsx
+++ src/components/ControlForm/ControlForm.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { screen, render, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { ControlForm } from "./ControlForm";
 import "@testing-library/jest-dom";
 
@@ -10,10 +11,7 @@ describe("Control form", () => {
     handleFilled: () => {},
     handleRunning: () => {},
     handleClear: () => {},
-    running: true,
-    size: 'small',
-    speed: 200,
-    filled: 0.25
+    running: true
   };
 
   test("render test", () => {
@@ -33,11 +31,31 @@ describe("Control form", () => {
     expect(handleRunning).toBeCalled();
   });
 
-  test("should call clear handler");
+  test("should call clear handler", () => {
+    const handleClear = jest.fn();
+    render(<ControlForm {...props} handleClear={handleClear} />);
+    fireEvent.click(screen.getByText(/clear/i));
+    expect(handleClear).toBeCalled();
+  });
 
-  test("should call size handler");
+  test("should call size handler", () => {
+    const handleSize = jest.fn();
+    render(<ControlForm {...props} handleSize={handleSize} />);
+    userEvent.selectOptions(screen.getByTestId("size"), "large");
+    expect(handleSize).toBeCalled();
+  });
 
-  test("should call speed handler");
+  test("should call speed handler", () => {
+    const handleSpeed = jest.fn();
+    render(<ControlForm {...props} handleSpeed={handleSpeed} />);
+    userEvent.selectOptions(screen.getByTestId("speed"), "fast");
+    expect(handleSpeed).toBeCalled();
+  });
 
-  test("should call filled handler");
+  test("should call filled handler", () => {
+    const handleFilled = jest.fn();
+    render(<ControlForm {...props} handleFilled={handleFilled} />);
+    userEvent.selectOptions(screen.getByTestId("filled"), "25%");
+    expect(handleFilled).toBeCalled();
+  });
 });
--- src/components/ControlForm/ControlForm.tsx
+++ src/components/ControlForm/ControlForm.tsx
@@ -8,30 +8,27 @@ interface Props {
   handleRunning: () => void;
   handleClear: () => void;
   running: boolean;
-  size: string;
-  speed: number;
-  filled: number;
 };
 
 export const ControlForm: FC<Props> = (props) => {
-  const { handleSize, handleSpeed, handleFilled, handleRunning, handleClear, running, size, speed, filled } = props;
+  const { handleSize, handleSpeed, handleFilled, handleRunning, handleClear, running } = props;
   return <>
     <BoxContainer>
       <form>
         <label htmlFor="size">Size:</label>
-        <select id="size" value={size} onChange={handleSize} disabled={!running}>
+        <select data-testid="size" id="size" defaultValue={"small"} onChange={handleSize} disabled={!running}>
           <option value="small">small</option>
           <option value="middle">middle</option>
           <option value="large">large</option>
         </select>
         <label htmlFor="speed">Speed:</label>
-        <select id="speed" value={speed} disabled={!running}>
+        <select data-testid="speed" id="speed" defaultValue={500} onChange={handleSpeed} disabled={!running}>
           <option value={800}>slow</option>
           <option value={500}>normal</option>
           <option value={200}>fast</option>
         </select>
         <label htmlFor="filled">Filled:</label>
-        <select id="filled" value={filled} disabled={!running}>
+        <select data-testid="filled" id="filled" defaultValue={0.1} onChange={handleFilled} disabled={!running}>
           <option value={0}>0</option>
           <option value={0.1}>10%</option>
           <option value={0.25}>25%</option>
@@ -43,6 +40,7 @@ export const ControlForm: FC<Props> = (props) => {
       <ButtonStyled onClick={handleRunning}>
         { running ? 'Stop' : 'Run' }
       </ButtonStyled>
+      <ButtonStyled onClick={handleClear}>Clear</ButtonStyled>
     </BoxContainer>
   </> ;
 };
