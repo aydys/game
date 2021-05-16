e197848fed447034da21dd54f16be7c761976001
aidys23 <aidongak23@gmail.com>
2021-05-16 13:18:14 +0700

    desrcibe scenarious for ControlForm

---
+++ src/components/ControlForm/ControlForm.test.tsx
@@ -0,0 +1,13 @@
+describe('Control form', () => {
+  test('render test');
+
+  test('should call running/stop handler');
+
+  test('should call clear handler');
+
+  test('should call size handler');
+
+  test('should call speed handler');
+
+  test('should call filled handler');
+});
