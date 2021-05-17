bafe9c1ae890cf1ef58c42458db1dc8310e4e369
aidys23 <aidongak23@gmail.com>
2021-05-17 22:08:30 +0700

    add clickField saga

--- src/module/saga.ts
+++ src/module/saga.ts
@@ -1,3 +1,18 @@
+import { takeEvery, put, select } from "redux-saga/effects";
+import { actions, PayloadField } from "./reducer";
+import { RootState } from "@/store";
+import { changeCellField } from "@utils";
+
+export const SelectorField = (state: RootState): boolean[][] => state.field;
+
+export function* clickField({
+  payload: { x, y, isFilled },
+}: PayloadField): Generator {
+  const field = (yield select(SelectorField)) as boolean[][];
+  const result = changeCellField(field, x, y, isFilled);
+  yield put({ type: actions.changeField.type, payload: result });
+}
+
 export function* gameSaga(): Generator {
-  return "Hello Saga";
+  yield takeEvery(actions.clickField.type, clickField);
 }
