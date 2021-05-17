c5427bf55bbcfbc900af3809e196123cdd9a7086
aidys <aidongak23@gmail.com>
2021-05-17 16:35:56 +0700

    add reducer test

---
+++ src/module/reducer.test.ts
@@ -0,0 +1,57 @@
+
+import { actions, reducer, initialState } from "./reducer";
+
+describe("Game actions", () => {
+  beforeEach(() => {
+    jest.spyOn(global.Math, "random").mockReturnValue(0.3);
+  });
+
+  afterEach(() => {
+    jest.spyOn(global.Math, "random").mockRestore();
+  });
+
+  it("should return the initial state", () => {
+    expect(reducer(undefined, { type: "" })).toEqual(initialState);
+  });
+
+  it("should create an action to click field", () => {
+    const payload = {
+      x: 1,
+      y: 0,
+      isFilled: false,
+    };
+    const expectedAction = {
+      type: actions.clickField.type,
+      payload,
+    };
+
+    expect(actions.clickField(payload)).toEqual(expectedAction);
+  });
+
+  it("should create an action to running game", () => {
+    const expectedAction = {
+      type: actions.runningGame.type,
+      payload: false
+    };
+    expect(actions.runningGame(false)).toEqual(expectedAction);
+  });
+
+  it("should change running of game", () => {
+    expect(reducer(initialState, { type: actions.runningGame.type, payload: false })).toEqual({
+      ...initialState,
+      running: false
+    });
+  });
+
+  it("should return initial state after click cell", () => {
+    expect(reducer(initialState, { type: actions.clickField.type, payload: false })).toEqual(initialState);
+  });
+
+  it("should create an action to change field", () => {
+    const expectedAction = {
+      type: actions.changeField.type,
+      payload: [[true]]
+    };
+    expect(actions.changeField([[true]])).toEqual(expectedAction);
+  });
+});
