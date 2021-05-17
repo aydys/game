cbdbf6b1c577c88113d61f22a6c7d773c3eeb148
aidys <aidongak23@gmail.com>
2021-05-17 12:30:23 +0700

    add test for generateInitField

---
+++ src/utils/generateInitField.test.ts
@@ -0,0 +1,28 @@
+import { shuffle, shuffleLineGrid, generateInitField } from "./generateInitField";
+describe("generateInitField" , () => {
+  it("shuffle array", () => {
+    const mock = [false, true, false];
+    const shuffledArray = shuffle(mock);
+    expect(shuffledArray).toEqual([true, false, false]);
+  });
+
+  it("shuffleLineGrid", () => {
+    const matchMock = [false, false, true];
+    const x = matchMock.length;
+    const per = 0.35;
+
+    const result = shuffleLineGrid(x, per);
+
+    expect(result).toEqual(matchMock);
+  });
+
+  it("generateInitField", () => {
+    const mock = [
+      [false, false, true],
+      [false, false, true],
+    ];
+    const result = generateInitField(3, 2, 0.35);
+
+    expect(result).toEqual(mock);
+  });
+})
