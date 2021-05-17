94e551f5ba52cb385515bfb52143b52e26f1260b
aidys <aidongak23@gmail.com>
2021-05-17 16:35:44 +0700

    add action creators

--- src/module/reducer.ts
+++ src/module/reducer.ts
@@ -1,3 +1,4 @@
+import { Action } from "redux";
 import { createSlice } from "@reduxjs/toolkit";
 import { generateInitField } from "@utils";
 
@@ -7,29 +8,40 @@ enum InitData {
   y = 15,
 }
 
+export interface PayloadField extends Action {
+  payload: {
+    x: number;
+    y: number;
+    isFilled: boolean;
+  };
+}
+
 interface GameState {
   field: boolean[][];
   size: "small" | "middle" | "large";
   speed: 200 | 500 | 800;
   filled: 0 | 0.1 | 0.25 | 0.5;
+  running: boolean
 }
 
-const initialState: GameState = {
+export const initialState: GameState = {
   field: generateInitField(InitData.x, InitData.y, InitData.filled),
   size: "middle",
   filled: 0.25,
   speed: 500,
+  running: false
 };
 
 const gameSlice = createSlice({
   name: "game",
   initialState,
   reducers: {
-    clickField: (state, { payload }) => {
+    changeField: (state, { payload }) => {
       state.field = payload;
     },
+    clickField: (state, _payload: PayloadField) => state,
     runningGame: (state, { payload }) => {
-      state.speed = payload;
+      state.running = payload;
     },
   },
 });
