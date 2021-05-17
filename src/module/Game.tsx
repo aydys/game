fe34b2234cdd83e056ed80e83799f209fe275f32
aidys <aidongak23@gmail.com>
2021-05-17 12:25:26 +0700

    include Field to Game

--- src/module/Game.tsx
+++ src/module/Game.tsx
@@ -1,5 +1,24 @@
 import React, { FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/store";
+import { actions } from "./reducer";
+
+import { CenterContainer, Header } from "@styled";
+import { Field } from "@/components/Field";
 
 export const Game: FC = () => {
-  return <></>;
+  const dispatch = useDispatch();
+  const running = useSelector((state: RootState) => state.running);
+  const field = useSelector((state: RootState) => state.field);
+
+  const clickCell = (x: number, y: number, isFilled: boolean) => {
+    dispatch(actions.clickField({ x, y, isFilled }));
+  };
+
+  return (
+    <CenterContainer>
+      <Header>Conway's Game of Life</Header>
+      <Field field={field} running={running} clickCell={clickCell} />
+    </CenterContainer>
+  );
 };
