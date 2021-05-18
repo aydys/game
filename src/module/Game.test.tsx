c8b92d978c9f9b11f2e814612435239433644a03
aidys <aidongak23@gmail.com>
2021-05-18 08:18:36 +0700

    use test-utils

--- src/module/Game.test.tsx
+++ src/module/Game.test.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import { Provider } from "react-redux";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import { store } from "@/store";
 import { Game } from "./Game";
+import { render, screen } from "./test-utils";
 
 describe("Game", () => {
   it("render test", () => {
-    render(
-      <Provider store={store}>
-        <Game />
-      </Provider>
-    );
+    render( <Game />);
 
     expect(screen.queryByText(/game of life/i)).toBeInTheDocument();
   });
