a6bcd7a4fb73c23daf0873f2f96ab750a30756f1
aidys23 <aidongak23@gmail.com>
2021-05-20 06:37:19 +0700

    test handlers to Game

--- src/module/Game.test.tsx
+++ src/module/Game.test.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import "@testing-library/jest-dom";
 import { Game } from "./Game";
 import { render, screen, waitFor } from "./test-utils";
-import { initialState } from "./reducer";
+import { actions, initialState } from "./reducer";
 import userEvent from "@testing-library/user-event";
+import * as redux from "react-redux";
 
 describe("check connected Game with redux", () => {
+  jest.mock("react-redux", () => ({
+    useDispatch: () => jest.fn,
+  }));
   it("render test", () => {
     render(<Game />, { initialState: { ...initialState, running: true } });
 
@@ -35,6 +39,73 @@ describe("check connected Game with redux", () => {
 
     await waitFor(() => {
       expect(screen.getByText(/run/i)).toBeInTheDocument();
+    });
+  });
+
+  it("should called action creator after user clicked clear button", () => {
+    const useDispatchSpy = jest.spyOn(redux, "useDispatch");
+    const mockDispatchFn = jest.fn();
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+    render(<Game />, {
+      initialState: { ...initialState, running: false },
+    });
+
+    userEvent.click(screen.getByText(/clear/i));
+
+    expect(mockDispatchFn).toHaveBeenCalledWith({
+      type: actions.clearField.type,
+    });
+  });
+
+  it("should called action creator after user select other size", () => {
+    const useDispatchSpy = jest.spyOn(redux, "useDispatch");
+    const mockDispatchFn = jest.fn();
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+    render(<Game />, {
+      initialState: { ...initialState, running: false, size: "small" },
+    });
+
+    userEvent.selectOptions(screen.getByTestId(/size/i), "large");
+
+    expect(mockDispatchFn).toHaveBeenCalledWith({
+      type: actions.changeSize.type,
+      payload: "large",
+    });
+  });
+
+  it("should called action creator after user select other speed", () => {
+    const useDispatchSpy = jest.spyOn(redux, "useDispatch");
+    const mockDispatchFn = jest.fn();
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+    render(<Game />, {
+      initialState: { ...initialState, running: false, speed: 800 },
+    });
+
+    userEvent.selectOptions(screen.getByTestId(/speed/i), "fast");
+
+    expect(mockDispatchFn).toHaveBeenCalledWith({
+      type: actions.changeSpeed.type,
+      payload: 200,
+    });
+  });
+
+  it("should called action creator after user select other filled", () => {
+    const useDispatchSpy = jest.spyOn(redux, "useDispatch");
+    const mockDispatchFn = jest.fn();
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+    render(<Game />, {
+      initialState: { ...initialState, running: false, speed: 0 },
+    });
+
+    userEvent.selectOptions(screen.getByTestId(/filled/i), "50%");
+
+    expect(mockDispatchFn).toHaveBeenCalledWith({
+      type: actions.changeFilled.type,
+      payload: 0.5,
     });
   });
 });
